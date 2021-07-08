69b0aabacd86d3ac13e472ba1b07a97eb1581ef5
Akshay Kumar <akshku01@in.ibm.com>
2021-07-09 04:29:43 +0530

    master branch first commit

---
+++ src/main/java/com/example/masterwhitespike/MasterWhitespikeApplication.java
@@ -0,0 +1,13 @@
+package com.example.masterwhitespike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterWhitespikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterWhitespikeApplication.class, args);
+    }
+
+}
