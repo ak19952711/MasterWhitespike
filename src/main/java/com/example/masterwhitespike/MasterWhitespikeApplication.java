2c6d3e61b2b9e843131b1f77b808cd1290a682b1
Akshay Kumar <akshku01@in.ibm.com>
2021-07-09 04:35:02 +0530

    master comment

--- src/main/java/com/example/masterwhitespike/MasterWhitespikeApplication.java
+++ src/main/java/com/example/masterwhitespike/MasterWhitespikeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MasterWhitespikeApplication {
-
+    //master branch
     public static void main(String[] args) {
         SpringApplication.run(MasterWhitespikeApplication.class, args);
     }
