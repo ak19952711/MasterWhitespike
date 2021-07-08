bd50c748bf280d25c77812ec8ccfa05dbaebcf67
Akshay Kumar <akshku01@in.ibm.com>
2021-07-09 04:57:08 +0530

    testing stored credentials

--- src/main/java/com/example/masterwhitespike/MasterWhitespikeApplication.java
+++ src/main/java/com/example/masterwhitespike/MasterWhitespikeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MasterWhitespikeApplication {
     //master branch
+    // testing something
     public static void main(String[] args) {
         SpringApplication.run(MasterWhitespikeApplication.class, args);
     }
