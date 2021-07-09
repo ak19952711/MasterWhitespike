eb517490586f03d0ef7802d439fdad82ee352692
Akshay Kumar <akshku01@in.ibm.com>
2021-07-09 05:42:08 +0530

    retest credless push

--- src/main/java/com/example/masterwhitespike/MasterWhitespikeApplication.java
+++ src/main/java/com/example/masterwhitespike/MasterWhitespikeApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MasterWhitespikeApplication {
     //master branch
-    // testing something
+    // testing something 2
     public static void main(String[] args) {
         SpringApplication.run(MasterWhitespikeApplication.class, args);
     }
